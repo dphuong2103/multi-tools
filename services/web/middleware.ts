89a36b05ab99ef535e0baf3b6ae2c4b67f45186d
dphuong2103 <midouz.project@gmail.com>
2024-10-14 22:43:08 +0700

    adjust middleware exclude robots.txt

--- services/web/middleware.ts
+++ services/web/middleware.ts
@@ -5,5 +5,5 @@ import { withI18nMiddleware } from "@/middlewares/intl-middleware";
 export default chain([withAuthMiddleware, withI18nMiddleware]);
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)"],
 };
